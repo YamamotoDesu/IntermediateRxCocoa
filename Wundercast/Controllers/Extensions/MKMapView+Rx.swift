2c1057156763999f6f3381d41a6c04aff9922807
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-18 13:26:24 +0900

    Creating the extension

--- Wundercast/Controllers/Extensions/MKMapView+Rx.swift
+++ Wundercast/Controllers/Extensions/MKMapView+Rx.swift
@@ -30,3 +30,45 @@ import Foundation
 import MapKit
 import RxSwift
 import RxCocoa
+
+extension MKMapView: HasDelegate {}
+
+class RxMKMapViewDelegateProxy: DelegateProxy<MKMapView, MKMapViewDelegate>, DelegateProxyType, MKMapViewDelegate {
+    
+    weak public private(set) var mapView: MKMapView?
+
+    public init(mapView: ParentObject) {
+      self.mapView = mapView
+      super.init(parentObject: mapView,
+                 delegateProxy: RxMKMapViewDelegateProxy.self)
+    }
+
+    static func registerKnownImplementations() {
+      register { RxMKMapViewDelegateProxy(mapView: $0) }
+    }
+
+
+}
+
+public extension Reactive where Base: MKMapView {
+    
+    var delegate: DelegateProxy<MKMapView, MKMapViewDelegate> {
+      RxMKMapViewDelegateProxy.proxy(for: base)
+    }
+
+    func setDelegate(_ delegate: MKMapViewDelegate) -> Disposable {
+      RxMKMapViewDelegateProxy.installForwardDelegate(
+        delegate,
+        retainDelegate: false,
+        onProxyForObject: self.base
+      )
+    }
+    
+    var overlay: Binder<MKOverlay> {
+      Binder(base) { mapView, overlay in
+        mapView.removeOverlays(mapView.overlays)
+        mapView.addOverlay(overlay)
+      }
+    }
+    
+}
--- Wundercast/ViewController.swift
+++ Wundercast/ViewController.swift
@@ -34,82 +34,198 @@ import UIKit
 import RxSwift
 import RxCocoa
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-  @IBOutlet private var mapView: MKMapView!
-  @IBOutlet private var mapButton: UIButton!
-  @IBOutlet private var geoLocationButton: UIButton!
-  @IBOutlet private var activityIndicator: UIActivityIndicatorView!
-  @IBOutlet private var searchCityName: UITextField!
-  @IBOutlet private var tempLabel: UILabel!
-  @IBOutlet private var humidityLabel: UILabel!
-  @IBOutlet private var iconLabel: UILabel!
-  @IBOutlet private var cityNameLabel: UILabel!
+    @IBOutlet private var mapView: MKMapView!
+    @IBOutlet private var mapButton: UIButton!
+    @IBOutlet private var geoLocationButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var searchCityName: UITextField!
+    @IBOutlet private var tempLabel: UILabel!
+    @IBOutlet private var humidityLabel: UILabel!
+    @IBOutlet private var iconLabel: UILabel!
+    @IBOutlet private var cityNameLabel: UILabel!
+    
+    private let bag = DisposeBag()
+    private let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        style()
+        
+        mapView.rx
+          .setDelegate(self)
+          .disposed(by: bag)
+        
+        let searchInput = searchCityName.rx
+            .controlEvent(.editingDidEndOnExit)
+            .map { self.searchCityName.text ?? "" }
+            .filter { !$0.isEmpty }
+        
+//        let search = searchInput
+//          .flatMapLatest { text in
+//            ApiController.shared
+//              .currentWeather(for: text)
+//              .catchErrorJustReturn(.empty)
+//          }
+//          .asDriver(onErrorJustReturn: .empty)
+        
+        let geoSearch = geoLocationButton.rx.tap
+          .flatMapLatest { _ in self.locationManager.rx.getCurrentLocation() }
+          .flatMapLatest { location in
+            ApiController.shared
+              .currentWeather(at: location.coordinate)
+              .catchErrorJustReturn(.empty)
+          }
+        
+        let textSearch = searchInput.flatMap { city in
+          ApiController.shared
+            .currentWeather(for: city)
+            .catchErrorJustReturn(.empty)
+        }
+        
+        let search = Observable
+          .merge(geoSearch, textSearch)
+          .asDriver(onErrorJustReturn: .empty)
 
-  private let bag = DisposeBag()
+        
+//        let running = Observable.merge(
+//            searchInput.map { _ in true },
+//            search.map { _ in false }.asObservable()
+//          )
+//          .startWith(true)
+//          .asDriver(onErrorJustReturn: false)
+        
+        let running = Observable.merge(
+          searchInput.map { _ in true },
+          geoLocationButton.rx.tap.map { _ in true },
+          search.map { _ in false }.asObservable()
+        )
+        .startWith(true)
+        .asDriver(onErrorJustReturn: false)
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    style()
 
-    let search = searchCityName.rx
-      .controlEvent(.editingDidEndOnExit)
-      .map { self.searchCityName.text ?? "" }
-      .filter { !$0.isEmpty }
-      .flatMapLatest { text in
-        ApiController.shared
-          .currentWeather(for: text)
-          .catchErrorJustReturn(.empty)
-      }
-      .asDriver(onErrorJustReturn: .empty)
+        running
+          .skip(1)
+          .drive(activityIndicator.rx.isAnimating)
+          .disposed(by: bag)
+        
+        running
+          .drive(tempLabel.rx.isHidden)
+          .disposed(by: bag)
 
-    search.map { "\($0.temperature)° C" }
-      .drive(tempLabel.rx.text)
-      .disposed(by: bag)
+        running
+          .drive(iconLabel.rx.isHidden)
+          .disposed(by: bag)
 
-    search.map(\.icon)
-      .drive(iconLabel.rx.text)
-      .disposed(by: bag)
+        running
+          .drive(humidityLabel.rx.isHidden)
+          .disposed(by: bag)
 
-    search.map { "\($0.humidity)%" }
-      .drive(humidityLabel.rx.text)
-      .disposed(by: bag)
+        running
+          .drive(cityNameLabel.rx.isHidden)
+          .disposed(by: bag)
 
-    search.map(\.cityName)
-      .drive(cityNameLabel.rx.text)
-      .disposed(by: bag)
-  }
 
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
+//        let search = searchCityName.rx
+//            .controlEvent(.editingDidEndOnExit)
+//            .map { self.searchCityName.text ?? "" }
+//            .filter { !$0.isEmpty }
+//            .flatMapLatest { text in
+//                ApiController.shared
+//                    .currentWeather(for: text)
+//                    .catchErrorJustReturn(.empty)
+//            }
+//            .asDriver(onErrorJustReturn: .empty)
+        
+        search.map { "\($0.temperature)° C" }
+            .drive(tempLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map(\.icon)
+            .drive(iconLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map { "\($0.humidity)%" }
+            .drive(humidityLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map(\.cityName)
+            .drive(cityNameLabel.rx.text)
+            .disposed(by: bag)
+        
+        search
+          .map { $0.overlay() }
+          .drive(mapView.rx.overlay)
+          .disposed(by: bag)
+        
+        geoLocationButton.rx.tap
+          .subscribe(onNext: { [weak self] _ in
+            guard let self = self else { return }
 
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+          })
+          .disposed(by: bag)
+        
+        locationManager.rx.didUpdateLocations
+          .subscribe(onNext: { locations in
+            print(locations)
+          })
+          .disposed(by: bag)
 
-    Appearance.applyBottomLine(to: searchCityName)
-  }
+        mapButton.rx.tap
+          .subscribe(onNext: {
+            self.mapView.isHidden.toggle()
+          })
+          .disposed(by: bag)
 
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        Appearance.applyBottomLine(to: searchCityName)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Style
+    
+    private func style() {
+        view.backgroundColor = UIColor.aztec
+        searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
+                                                                  attributes: [.foregroundColor: UIColor.textGrey])
+        searchCityName.textColor = UIColor.ufoGreen
+        tempLabel.textColor = UIColor.cream
+        humidityLabel.textColor = UIColor.cream
+        iconLabel.textColor = UIColor.cream
+        cityNameLabel.textColor = UIColor.cream
+    }
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+extension ViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView,
+               rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+    guard let overlay = overlay as? ApiController.Weather.Overlay else {
+      return MKOverlayRenderer()
+    }
 
-  // MARK: - Style
-
-  private func style() {
-    view.backgroundColor = UIColor.aztec
-    searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
-                                                              attributes: [.foregroundColor: UIColor.textGrey])
-    searchCityName.textColor = UIColor.ufoGreen
-    tempLabel.textColor = UIColor.cream
-    humidityLabel.textColor = UIColor.cream
-    iconLabel.textColor = UIColor.cream
-    cityNameLabel.textColor = UIColor.cream
+    return ApiController.Weather.OverlayView(overlay: overlay,
+                                             overlayIcon: overlay.icon)
   }
 }
+
